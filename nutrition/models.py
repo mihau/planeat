f18a2683414bb7f0612aa2b6089f40400199ef99
Michał Szymański <mszymanski@opera.com>
2016-11-07 23:18:07 +0100

    add USDA nutrient database and a png schema

--- nutrition/models.py
+++ nutrition/models.py
@@ -11,6 +11,9 @@ class Nutrient(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Edible(models.Model):
     """ This class describes anything that can be eaten.
@@ -19,6 +22,15 @@ class Edible(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     _calories = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def calories(self):
+        return self._calories or sum(
+            [i.amount * i.ingredient.calories for i in self.ingredients.all()]
+        )/100
 
 
 class Recipe(Edible):
@@ -44,6 +56,9 @@ class EdibleIngredients(models.Model):
     )
     amount = models.FloatField()
 
+    def __str__(self):
+        return "{} grams of {}".format(self.amount, self.ingredient.name)
+
 
 class NutrientContent(models.Model):
     """ Objects of this class store information about content of nutrients
@@ -53,3 +68,6 @@ class NutrientContent(models.Model):
     edible = models.ForeignKey('Edible', on_delete=models.CASCADE)
     nutrient = models.ForeignKey('Nutrient', on_delete=models.CASCADE)
     content = models.FloatField()
+
+    def __str__(self):
+        return "{} grams of {}".format(self.content, self.nutrient.name)
